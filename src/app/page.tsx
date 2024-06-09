3a495ac875d6fe7fcd063720251c6fa18f78d6ef
yumzen <yeom.ji.eun35@gmail.com>
2024-06-09 20:36:02 +0900

    routing 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,30 @@ import LogoComponent from '../components/Logo';
 import Navigationbar from '@/components/layout/Navigationbar';
 import dynamic from 'next/dynamic';
 import Layout from './layout';
+import {usePathname } from 'next/navigation';
 
 const Login = dynamic(() => import('../components/Login'), { ssr: false });
-const HomePage = () => (
+const HomePage = () => {
+  const pathname = usePathname();
+
+  const isHomePage = pathname === "/home";
+
+  const isCafePage = pathname === "/cafe";
+
+  const isMapPage = pathname === "/map";
+
+  const isMyPage = pathname === "/mypage";
+
+  return(
   <Layout>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </Head>
     <LogoComponent />
-    <Navigationbar />
+    {(isHomePage || isCafePage || isMapPage || isMyPage) && <Navigationbar />}
     <Login />
   </Layout>
-);
+  );
+};
 
 export default HomePage;
