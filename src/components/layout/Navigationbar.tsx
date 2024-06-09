4e67168b365e02a3ad1e0e160f6dbd4905951d8c
yumzen <yeom.ji.eun35@gmail.com>
2024-06-09 20:17:19 +0900

    Feat: Navbar 구현

--- src/components/layout/Navigationbar.tsx
+++ src/components/layout/Navigationbar.tsx
@@ -1,12 +1,25 @@
 import Image from 'next/image';
 import styled from 'styled-components';
-
+import Link from "next/link";
 const Navigationbar = () => {
-
     return (
         <nav>
             <Container>
                 <Navbar>
+                    <Buttons>
+                        <StyledLink href="home">
+                            <Image src="/assets/images/layout/home.svg" alt="home" width={30} height={41}/>
+                        </StyledLink>
+                        <StyledLink href="cafe">
+                            <Image src="/assets/images/layout/cafe.svg" alt="cafe" width={30} height={41}/>
+                        </StyledLink>
+                        <StyledLink href="map">
+                            <Image src="/assets/images/layout/map.svg" alt="map" width={30} height={41}/>
+                        </StyledLink>
+                        <StyledLink href="mypage">
+                            <Image src="/assets/images/layout/my.svg" alt="my" width={30} height={41}/>
+                        </StyledLink>
+                    </Buttons>
                 </Navbar>
             </Container>
         </nav>
@@ -16,6 +29,7 @@ const Navigationbar = () => {
 export default Navigationbar;
 
 const Container = styled.div`
+    position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -24,7 +38,24 @@ const Container = styled.div`
 const Navbar = styled.div`
     width: 393px;
     height: 84px;
+    display: flex;
     flex-shrink: 0;
     border-top: 1px solid #E7E7E7;
     background: #FFF;
+    justify-content: center;
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap:11px;
+    flex-shrink: 0;
+`;
+
+const StyledLink = styled(Link)`
+    width: 90px;
+    display: flex;
+    justify-content: center;
 `;
