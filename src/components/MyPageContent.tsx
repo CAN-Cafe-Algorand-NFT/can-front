7d1f5ec1d7a99226061d1558d4cb5d3aee688d21
junseom <quasar125@naver.com>
2024-06-10 01:43:18 +0900

    Feat: mypage 세부사항

--- src/components/MyPageContent.tsx
+++ src/components/MyPageContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import AccountInfo from '../components/AccountInfo';
+import { useRouter } from 'next/router';
+import AccountInfo from './AccountInfo';
 
 const ContentContainer = styled.div`
   padding: 20px;
@@ -17,13 +18,15 @@ const SectionTitle = styled.div`
 
 const NFTGrid = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 10px;
 `;
 
 const NFTItem = styled.div`
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   background-color: #ccc;
+  cursor: pointer;
 `;
 
 const Link = styled.a`
@@ -33,34 +36,52 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
-const MyPageContent = () => (
-  <ContentContainer>
-    <Section>
-    <AccountInfo />
-    </Section>
-    <Section>
-      <SectionTitle>My NFT</SectionTitle>
-      <NFTGrid>
-        <NFTItem />
-        <NFTItem />
-        <NFTItem />
-      </NFTGrid>
-    </Section>
-    <Section>
-      <SectionTitle>Like</SectionTitle>
-      <NFTGrid>
-        <NFTItem />
-        <NFTItem />
-        <NFTItem />
-      </NFTGrid>
-    </Section>
-    <Link>Go to purchase NFT</Link>
-    <Section>
-      <Link>Setting</Link>
-      <Link>Service center</Link>
-      <Link>Log out</Link>
-    </Section>
-  </ContentContainer>
-);
+const MyPageContent: React.FC = () => {
+  const router = useRouter();
+
+  const handleNFTClick = (nftId: string) => {
+    router.push(`/mypage/nft/${nftId}`);
+  };
+
+  const handleLikeClick = (likeId: string) => {
+    router.push(`/mypage/like/${likeId}`);
+  };
+
+  return (
+    <ContentContainer>
+      <Section>
+        <AccountInfo />
+      </Section>
+      <Section>
+        <SectionTitle>My NFT</SectionTitle>
+        <NFTGrid>
+          {["1", "2", "3"].map((nftId) => (
+            <NFTItem key={nftId} onClick={() => handleNFTClick(nftId)}>
+              NFT {nftId}
+            </NFTItem>
+          ))}
+        </NFTGrid>
+      </Section>
+      <Section>
+        <SectionTitle>Like</SectionTitle>
+        <NFTGrid>
+          {["1", "2", "3", "4"].map((likeId) => (
+            <NFTItem key={likeId} onClick={() => handleLikeClick(likeId)}>
+              {/* <img src={`/images/cafe${likeId}.jpg`} alt={`Cafe ${likeId}`} /> */}
+              <div>Cafe Name</div>
+              <div>Anam 182-1, Seoul</div>
+            </NFTItem>
+          ))}
+        </NFTGrid>
+      </Section>
+      <Link>Go to purchase NFT</Link>
+      <Section>
+        <Link>Setting</Link>
+        <Link>Service center</Link>
+        <Link>Log out</Link>
+      </Section>
+    </ContentContainer>
+  );
+};
 
 export default MyPageContent;
